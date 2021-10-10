650fbd9ab7ee2099bcc95ebfcb6270050b9bd721
Tigist Diriba <tigist.diriba@flyreel.co>
2021-10-10 09:33:38 -0400

    Update requirements and configs

--- sesemi/models/heads/__init__.py
+++ sesemi/models/heads/__init__.py
@@ -2,4 +2,3 @@
 # Copyright 2021, Flyreel. All Rights Reserved.
 # =============================================#
 """Head models."""
-from . import base, resnet, timm
--- sesemi/schedulers/weight.py
+++ sesemi/schedulers/weight.py
@@ -35,3 +35,20 @@ class SigmoidRampupScheduler(WeightScheduler):
 
     def __call__(self, step: int) -> float:
         return self.weight * sigmoid_rampup(step, self.stop_rampup)
+
+
+class SigmoidRampdownScheduler(WeightScheduler):
+    """A sigmoid ramp-down weight scheduler."""
+
+    def __init__(self, weight: float, stop_rampdown: int):
+        """Initializes the weight scheduler.
+
+        Args:
+            weight: The weight to apply to the sigmoidal function.
+            stop_rampdown: The step to stop ramping down the output weight.
+        """
+        self.weight = weight
+        self.stop_rampdown = stop_rampdown
+
+    def __call__(self, step: int) -> float:
+        return self.weight * (1.0 - sigmoid_rampup(step, self.stop_rampdown))

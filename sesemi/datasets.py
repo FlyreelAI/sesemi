0d84f48d307980831b042baa7acd2c1653b4abab
tigist-d <75636216+tigist-d@users.noreply.github.com>
2021-12-03 19:23:35 -0500

    Refactor CIFAR-10, CIFAR-100, and STL-10 datasets and models (#29)

--- sesemi/datasets.py
+++ sesemi/datasets.py
@@ -5,9 +5,9 @@
 import os
 
 from torch.utils.data import ConcatDataset, Dataset, IterableDataset
-from torchvision.datasets import ImageFolder
+from torchvision.datasets import ImageFolder, CIFAR10, CIFAR100, STL10
 
-from typing import Callable, Dict, List, Optional, Union
+from typing import Callable, Dict, List, Optional, Type, Union
 
 DatasetBuilder = Callable[..., Union[Dataset, IterableDataset]]
 ImageTransform = Callable
@@ -61,6 +61,165 @@ def image_folder(
 
         dsts = [
             ImageFolder(os.path.join(root, s), transform=image_transform)
+            for s in subsets
+        ]
+
+        return ConcatDataset(dsts)
+
+
+def _cifar(
+    dataset_cls: Union[Type[CIFAR10], Type[CIFAR100]],
+    root: str,
+    subset: Optional[Union[str, List[str]]] = None,
+    image_transform: Optional[ImageTransform] = None,
+    **kwargs,
+) -> Dataset:
+    """A CIFAR dataset builder.
+
+    Args:
+        dataset_cls: One of (CIFAR10, CIFAR100).
+        root: The path to the image folder dataset.
+        subset: The subset(s) to use. Subets include (train, test).
+        image_transform: The image transformations to apply.
+
+    Returns:
+        An `ImageFolder` dataset.
+    """
+    if isinstance(subset, str):
+        assert subset in {
+            "train",
+            "test",
+        }, f"invalid subset {subset}, only support {{train, test}}"
+        train = subset == "train"
+        return dataset_cls(
+            root,
+            train=train,
+            download=True,
+            transform=image_transform,
+        )
+    else:
+        if subset is None:
+            subsets = ["train", "test"]
+        else:
+            subsets = subset
+
+        for s in subsets:
+            assert s in {
+                "train",
+                "test",
+            }, f"invalid subset {subset}, only support {{train, test}}"
+
+        dsts = [
+            dataset_cls(
+                root,
+                train=(s == "train"),
+                download=True,
+                transform=image_transform,
+            )
+            for s in subsets
+        ]
+
+        return ConcatDataset(dsts)
+
+
+@register_dataset
+def cifar10(
+    root: str,
+    subset: Optional[Union[str, List[str]]] = None,
+    image_transform: Optional[ImageTransform] = None,
+    **kwargs,
+) -> Dataset:
+    """A CIFAR-10 dataset builder.
+
+    Args:
+        root: The path to the image folder dataset.
+        subset: The subset(s) to use. Subets include (train, test).
+        image_transform: The image transformations to apply.
+
+    Returns:
+        An `ImageFolder` dataset.
+    """
+    return _cifar(
+        CIFAR10, root, subset=subset, image_transform=image_transform, **kwargs
+    )
+
+
+@register_dataset
+def cifar100(
+    root: str,
+    subset: Optional[Union[str, List[str]]] = None,
+    image_transform: Optional[ImageTransform] = None,
+    **kwargs,
+) -> Dataset:
+    """A CIFAR-100 dataset builder.
+
+    Args:
+        root: The path to the image folder dataset.
+        subset: The subset(s) to use. Subets include (train, test).
+        image_transform: The image transformations to apply.
+
+    Returns:
+        An `ImageFolder` dataset.
+    """
+    return _cifar(
+        CIFAR100, root, subset=subset, image_transform=image_transform, **kwargs
+    )
+
+
+@register_dataset
+def stl10(
+    root: str,
+    subset: Optional[Union[str, List[str]]] = None,
+    image_transform: Optional[ImageTransform] = None,
+    **kwargs,
+) -> Dataset:
+    """An STL10 dataset builder.
+
+    Args:
+        root: The path to the image folder dataset.
+        subset: The subset(s) to use. Subets include
+            (train, test, unlabeled, train+unlabeled).
+        image_transform: The image transformations to apply.
+
+    Returns:
+        An `ImageFolder` dataset.
+    """
+    if isinstance(subset, str):
+        assert subset in {
+            "train",
+            "test",
+            "unlabeled",
+            "train+unlabeled",
+        }, f"invalid subset {subset}, only support {{train, test, unlabeled, train+unlabeled}}"
+        return STL10(
+            root,
+            split=subset,
+            download=True,
+            transform=image_transform,
+            **kwargs,
+        )
+    else:
+        if subset is None:
+            subsets = ["train", "test", "unlabeled"]
+        else:
+            subsets = subset
+
+        for s in subsets:
+            assert s in {
+                "train",
+                "test",
+                "unlabeled",
+                "train+unlabeled",
+            }, f"invalid subset {subset}, only support {{train, test, unlabeled, train+unlabeled}}"
+
+        dsts = [
+            STL10(
+                root,
+                split=s,
+                download=True,
+                transform=image_transform,
+                **kwargs,
+            )
             for s in subsets
         ]
 
--- sesemi/models/backbones/__init__.py
+++ sesemi/models/backbones/__init__.py
@@ -2,3 +2,4 @@
 # Copyright 2021, Flyreel. All Rights Reserved.
 # =============================================#
 """Backbone models."""
+from . import base, resnet, timm
